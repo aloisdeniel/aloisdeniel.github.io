79edf280c0dda925fad796d679088cf30ac86019
Aloïs Deniel <alois.deniel@outlook.com>
2014-05-31 18:46:21 +0200

    Contact page
    Adds the contact elements.

---
+++ src/build.js
@@ -0,0 +1,21 @@
+var wintersmith = require('wintersmith');
+
+var env = wintersmith({'port' : 29600});
+
+// build site
+env.build(function(error) {
+  if (error) throw error;
+  console.log('Done!');
+});
+
+// preview
+env.preview(function(error, server) {
+  if (error) throw error;
+  console.log('Server running!');
+});
+
+// do something with the content tree
+env.load(function(error, result) {
+  if (error) throw error;
+  console.log('Contents loaded :' + result);
+});
