78f57379f074d2e975578edea01dcbbbd91ecc7f
Alois Deniel <alois.deniel@gmail.com>
2016-02-08 13:49:51 +0100

    Started the re-design.

--- src/components/icon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" :style="{ width: size + 'px', width: size + 'px' }" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path class="contact-icon" :d="shape | shape" :fill="color" fill-rule="nonzero"/></svg>
-</template>
-
-<script>
-module.exports = {
-  props: [ 'size', 'color', 'shape']
-};
-</script>
