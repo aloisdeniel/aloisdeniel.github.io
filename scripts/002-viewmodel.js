c36e5b845b8b29eb9fa40c7099868b9a265163de
Alois Deniel <alois.deniel@gmail.com>
2016-02-11 14:32:10 +0100

    Fixe design misalignments, started competences descriptions.

--- scripts/002-viewmodel.js
+++ scripts/002-viewmodel.js
@@ -51,7 +51,7 @@ opensource:[
                   link: "https://github.com/aloisdeniel/CommonExtensions"
             },
             {
-                  title: "Xamarin.Animtations",
+                  title: "Xamarin.Animations",
                   link: "https://github.com/aloisdeniel/Xamarin.Animations"
             }
         ]
@@ -86,23 +86,23 @@ competences: [
       [
             {
                   title: "xamarin",
-                  description: "Blah blah blah",
+                  description: "Initiator of the use of the technology into the company which led to a  major partnership, company technical leader",
                   type: "primary",
                   logo: "logo_xamarin.png",
-                  tools: ["Visual Studio","Xamarin Studio", "C#", "UITests"],
+                  tools: ["Visual Studio","Xamarin Studio", "TFS", "C#", "UITests"],
                   circles: [1,1,1,1]
             },
             {
                   title: "windows platform",
-                  description: "Blah blah blah",
+                  description: "Company technical leader, in charge of mobile continuous integration tools, and project technical supervision",
                   type: "primary",
                   logo: "logo_windows.png",
-                  tools: ["Visual Studio","WinRT", "C#", "XAML"],
+                  tools: ["Visual Studio", "TFS", "WinRT", "C#", "XAML"],
                   circles: [1,1,1,1]
             },
             {
                   title: "ios",
-                  description: "Blah blah blah",
+                  description: "Lead developer on native and Xamarin applications",
                   type: "primary",
                   logo: "logo_apple.png",
                   tools: [ "Objective-C", "Swift 2", "XCode","CocoaPods"],
@@ -112,7 +112,7 @@ competences: [
       [
             {
                   title: "node.js",
-                  description: "Blah blah blah \n blah blah",
+                  description: "Several professionnal projects for internal enterprise tools, opensource projects",
                   type: "secondary",
                   logo: "logo_nodejs.png",
                   tools: ["js", "npm", "express.js", "koa.js"],
@@ -120,7 +120,7 @@ competences: [
             },
             {
                   title: "design",
-                  description: "Blah blah blah",
+                  description: "Designer for several mobile applications and websites",
                   type: "secondary",
                   logo: "logo_ps.png",
                   tools: ["Photoshop"],
@@ -128,7 +128,7 @@ competences: [
             },
             {
                   title: "android",
-                  description: "Blah blah blah",
+                  description: "Professional training, one production application made Xamarin.Android",
                   type: "secondary",
                   logo: "logo_android.png",
                   tools: ["Android Studio","Gradle"],
